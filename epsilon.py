5b6238dd272286263617d19570880f868c1e9630
genomexyz <newmr.cyberyan@yahoo.com>
2017-01-01 21:49:52 +0700

    Update epsilon.py

--- epsilon.py
+++ epsilon.py
@@ -5,14 +5,16 @@ import numpy
 
 maks = 1.0
 mins = 0.0
+frag = 0
 
 while True:
+	frag = frag + 1
 	test = (maks + mins) / 2.0
 	if (1.0 + test) != 1.0:
 		maks = test
 	else:
 		maks = test
 		break
-	if (1.0 + test) == 1.0:
-		mins = test
 print 'the epsilon is %.50f' % (maks)
+print 'penjumlahan 1 + epsilon = %.50f' % (1+maks)
+print 'bentuk pecahan dari epsilon adalah 1/2^%i' % (frag)
